e9149b9a5fd9de4ebc0070f9d0312955c10a856c
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-07 22:19:14 +0000

    Prepare project for development.

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static("public"));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
+
+module.exports = app;
